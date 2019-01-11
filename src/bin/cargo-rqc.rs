146739c3a185b5682ee25c38ff846cd4264d366e
Brian L. Troutwine <brian@troutwine.us>
2019-01-10 22:34:55 -0800

    fork/execv target running model now working
    As of this commit it's possible for rqc to be given a target to run --
    without arguments -- and it'll do that. There's a stub based on shm
    for intra-process communication and that'll be the next bit of work
    that happens.
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- src/bin/cargo-rqc.rs
+++ src/bin/cargo-rqc.rs
@@ -1,8 +1,9 @@
 extern crate clap;
 extern crate rqc;
 
-use clap::{App, AppSettings, SubCommand};
-use rqc::RqcBuild;
+use clap::{App, AppSettings, Arg, SubCommand};
+use rqc::Rqc;
+use std::path::PathBuf;
 
 fn main() {
     let app = App::new("cargo-rqc")
@@ -15,11 +16,38 @@ fn main() {
                 .about("Build all test targets")
                 .before_help("TODO")
                 .after_help("TODO"),
+        )
+        .subcommand(
+            SubCommand::with_name("run")
+                .about("Run a test target")
+                .arg(
+                    Arg::with_name("target")
+                        .short("t")
+                        .long("target")
+                        .value_name("TARGET")
+                        .help("path to the test target")
+                        .takes_value(true),
+                )
+                .before_help("TODO")
+                .after_help("TODO"),
         );
     let args = app.get_matches();
 
     match args.subcommand() {
-        ("build", matches) => RqcBuild::new().build(matches.expect("arguments present")),
+        ("build", _matches) => Rqc::new().build(),
+        ("run", matches) => {
+            let target = PathBuf::from(
+                matches
+                    .expect("run must have arguments")
+                    .value_of("target")
+                    .expect("must supply a target"),
+            );
+            if !target.exists() {
+                panic!("given target does not exist on disk");
+            }
+
+            Rqc::new().run(target.as_path())
+        }
         (s, _) => panic!("unimplemented subcommand {}!", s),
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,24 @@
-extern crate clap;
+extern crate libc;
+extern crate nix;
 
-use clap::ArgMatches;
+use nix::errno::Errno;
+use nix::fcntl::OFlag;
+use nix::sys::mman::{mmap, shm_open, shm_unlink, MapFlags, ProtFlags};
+use nix::sys::stat::Mode;
+use nix::sys::wait::{waitpid, WaitPidFlag, WaitStatus};
+use nix::unistd::{execv, fork, ftruncate, ForkResult};
+use std::ffi::CString;
+use std::os::unix::io::RawFd;
+use std::path::Path;
 
-pub struct RqcBuild {}
+pub struct Rqc {}
 
-impl RqcBuild {
+impl Rqc {
     pub fn new() -> Self {
-        RqcBuild {}
+        Self {}
     }
 
-    pub fn build(&self, _args: &ArgMatches) -> () {
+    pub fn build(&self) -> () {
         let cargo_path = env!("CARGO");
 
         let mut rustflags: String = "-C debug-assertions \
@@ -30,5 +39,92 @@ impl RqcBuild {
 
         let status = cmd.env("RUSTFLAGS", &rustflags).status().unwrap();
         ::std::process::exit(status.code().unwrap_or(1));
+    }
+
+    pub fn run(&self, target: &Path) -> () {
+        let def_file_mode = Mode::S_IRUSR
+            | Mode::S_IWUSR
+            | Mode::S_IRGRP
+            | Mode::S_IWGRP
+            | Mode::S_IROTH
+            | Mode::S_IWOTH;
+        let shm_path = "/RQC";
+        let memfd = shm_open(shm_path, OFlag::O_CREAT | OFlag::O_RDWR, def_file_mode)
+            .expect("failed to open shared memory");
+        if let Err(e) = ftruncate(memfd, 1024) {
+            shm_unlink(shm_path).expect("failed to unlink opened shm");
+            println!("could not truncate shared memory to appropriate size");
+            ::std::process::exit(1);
+        }
+        let ptr = unsafe {
+            mmap(
+                0 as *mut libc::c_void,
+                1024,
+                ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+                MapFlags::MAP_SHARED,
+                memfd,
+                0,
+            )
+            .expect("could not memory map shared memory file")
+        };
+        // NOTE(blt) -- okay, now, at this point we actually need to fork/exec
+        // and get a child process to do a similar mmap dance. Also, we should
+        // hide this inside a proper type. Initial goal: just get a handshake
+        // going between the client (test target) and server (test runner).
+
+        let c_path = CString::new(
+            target
+                .as_os_str()
+                .to_str()
+                .expect("path contains invalid unicode characters"),
+        )
+        .expect("unable to coerce path into c-style string");
+
+        match fork() {
+            Ok(ForkResult::Parent { child, .. }) => {
+                let mut st = 0;
+                loop {
+                    match waitpid(child, Some(WaitPidFlag::WNOHANG | WaitPidFlag::WUNTRACED)) {
+                        Ok(status) => match status {
+                            WaitStatus::StillAlive => {
+                                continue;
+                            }
+                            WaitStatus::Exited(_, exit_status) => {
+                                if exit_status != 0 {
+                                    println!("target exited with non-zero status: {}", exit_status);
+                                    st = exit_status;
+                                } else {
+                                    continue;
+                                }
+                            }
+                            s => {
+                                println!("target finished with status: {:?}", s);
+                            }
+                        },
+                        Err(e) => match e.as_errno() {
+                            Some(Errno::ECHILD) => {}
+                            _ => {
+                                println!("waiting on target failed with: {}", e);
+                                st = 1;
+                            }
+                        },
+                    }
+                    break;
+                }
+                shm_unlink(shm_path).expect("failed to unlink opened shm");
+                ::std::process::exit(st);
+            }
+            Ok(ForkResult::Child) => {
+                // TODO(blt) for some reason the args aren't getting passed to the child
+                execv(&c_path, &[c_path.clone(), CString::new(shm_path).unwrap()])
+                    .expect("could not execv");
+            }
+            Err(_) => {
+                println!("Unable to fork target");
+                ::std::process::exit(1);
+            }
+        }
+        // execv(&c_path, &[c_path.clone(), CString::new(shm_path).unwrap()])
+        //     .expect("could not execv");
     }
 }
